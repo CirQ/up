022bd36bc92e562a95b2e26d411547d0b6965d26
wsn <wangsn@mail.sustech.edu.cn>
2023-02-28 21:04:37 +0800

    init

---
+++ build.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# prepare config file
+token=$(uuidgen)
+sed "s/####/$token/g" config_template.json > config.json
+
+# run docker server
+docker pull v2fly/v2fly-core
+docker run -d --name v2-server -v .:/etc/v2ray -p 54321:54321 --restart always v2fly/v2fly-core run -c /etc/v2ray/config.json
+
+
+
+
+
+echo $token
